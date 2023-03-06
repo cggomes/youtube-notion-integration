7293593b9e87c843e8f6590f5992d2e02561e796
Christian Gomes <christiangomes00@gmail.com>
2023-03-06 20:44:16 -0300

    Changing API URL from Heroku to Vercel

--- client/src/environments/environment.prod.ts
+++ client/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  API_URL: "https://youtube-notion.herokuapp.com/playlists",
+  API_URL: "https://youtube-notion-integration-66a8m4jl6-christiangomes00.vercel.app/playlists",
 };
